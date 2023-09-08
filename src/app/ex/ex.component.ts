c8826ae590ef5ccc8c058e7be704218219903234
viswavenkat0707 <142783490+viswavenkat0707@users.noreply.github.com>
2023-09-08 17:18:06 +0530

    rahul is a dev

--- src/app/ex/ex.component.ts
+++ src/app/ex/ex.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as AOS from 'aos'; 
 
 @Component({
   selector: 'app-ex',
   templateUrl: './ex.component.html',
   styleUrls: ['./ex.component.css']
 })
-export class ExComponent {
- 
+export class ExComponent implements OnInit {
+  ngOnInit() { AOS.init(); } 
 }
 
 
